d51ab6104fecc20bd7fb3477d416224d464dfe94
David Kopala <kopala.david@gmail.com>
2019-05-17 11:30:19 -0600

    Finished double_for_py

--- double_for_py.py
+++ double_for_py.py
@@ -6,7 +6,9 @@ from math import cos, sin, pi
 sys.path.append(os.path.abspath('/home/pi/scoreboard/scoreboard'))
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 
-class test_class:
+from samplebase import SampleBase
+
+class test_class:#(SampleBase):
     def __init__(self, *args, **kwargs):
         o = RGBMatrixOptions()
         o.rows = 16
@@ -18,33 +20,17 @@ class test_class:
         o.pwm_lsb_nanoseconds = 100
         o.brightness = 25
         self.matrix = RGBMatrix(options=o)
+        # super(test_class, self).__init__(*args, **kwargs)
 
     def run(self):
         print "Running Test!"
-        center_x = self.matrix.width // 2
-        center_y = self.matrix.height // 2
 
         canvas = self.matrix.CreateFrameCanvas()
-        canvas.Fill(0, 0, 255)
-        
-        radius_max = canvas.width / 2.0
-        angle_step = 1 / 360.0
-
-        a = 0
-        r = 0
-        while r < center_y:
-            dot_x = cos(a * 2 * pi) * r
-            dot_y = sin(a * 2 * pi) * r
-
-            canvas.SetPixel(center_x + dot_x, center_y + dot_y, 255, 0, 0)
-            
-            canvas = self.matrix.SwapOnVSync(canvas)
-
-            print "x: %i; y: %i" % (center_x + dot_x, center_y + dot_y)
-
-            # sleep(0.1)
-            a += angle_step
-            r += angle_step
+        for y in range(self.matrix.height):
+            for x in range(self.matrix.width):
+                canvas.SetPixel(x,y,255,255,255)
+                print "(%i, %i)" % (x, y)
+                canvas = self.matrix.SwapOnVSync(canvas)
 
 if __name__ == "__main__":
     test = test_class()
